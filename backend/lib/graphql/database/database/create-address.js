d08cb15bb61d685abf2d29f89e62355800b506aa
Omar Shibli <omarshib@gmail.com>
2017-10-13 17:14:29 +0300

    refactoring: added address-validator util

--- backend/lib/graphql/database/database/create-address.js
+++ backend/lib/graphql/database/database/create-address.js
@@ -1,77 +1,20 @@
 
 // imports
-import { find } from 'lodash'
+import { find, isString } from 'lodash'
 
 // local imports
 import { loadEvents, saveEvent } from '../../../../lib/events-store';
 import { createOrderedId } from '../../../../lib/uuid';
 import { event_type, address_status } from '../../../../model/consts';
-// dependencies to check addresses ******** TO EXPORT IN OTHER FILE TOGETHER WITH ADDRESS CHECKING LOGIC
-const WAValidator = require('wallet-address-validator');
-const SHA3 = require('crypto-js/sha3');
-
-
-// I left this code inside this file, but it would be nice to export all the checking logic and import it in a single function checkEthBtcAddress().
-// **********************TO EXPORT*****************************************************
-// this is a dependency to check eth address
-let sha3 = (value) => {
-  return SHA3(value, {
-    outputLength: 256
-  }).toString();
-}
-
-// this is the ethereum address checker. it also checks for checksums addresses
-let isEtherAddress = (address) => {
-  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-    // Check if it has the basic requirements of an address
-    return false;
-  }
-  else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
-    // If it's all small caps or all all caps, return true
-    return true;
-  }
-  else {
-    // Otherwise check each case
-    return isChecksumAddress(address);
-  }
-};
-
-// this function completes the isEtherAddress() function.
-let isChecksumAddress = function (address) {
-  // Check each case
-  address = address.replace('0x','');
-  let addressHash = sha3(address.toLowerCase());
-
-  for (let i = 0; i < 40; i++ ) {
-    // The nth letter should be uppercase if the nth digit of casemap is 1
-    if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) ||
-      (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// this function combines the btc check with the ethereum check
-const checkEthBtcAddress = (address) => {
-  if (address[0] !== '0' && address[1] !== 'x') {
-    if (!WAValidator.validate(address, 'BTC', 'both')) {
-      throw 'Bitcoin Address not valid.';
-    }
-  } else if (!isEtherAddress(address)){
-    throw 'Ethereum Address not valid.';
-  }
-}
-// ***************************************************************************
-
+import { checkEthBtcAddress } from '../../../address-validator';
 
 export default async (addressInput) => {
   const address = addressInput.address
   // if length > 50, we throw immediatelly
-  if(address.length > 50) {
+  if(isString(address) && address.length > 50) {
     throw "Address length is not valid."
   }
-  
+
   // Otherwise, we go to check the address
   checkEthBtcAddress(address)
 
